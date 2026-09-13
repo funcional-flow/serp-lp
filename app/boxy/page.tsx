3a89239aeeb7aa3ddab0008955a6af9ce4a3fee0
DoRenato <64191958+DoRenato@users.noreply.github.com>
2026-09-13 20:35:55 -0300

    Update boxy page

--- app/boxy/page.tsx
+++ app/boxy/page.tsx
@@ -1,20 +1,9 @@
-import { ScrollSections } from "@/components/gsap/ScrollSections";
+
 
 export default function page() {
   return (
     <div className="flex flex-col">
-      <ScrollSections
-        content={[
-          <div key="1" className="h-svh w-full bg-red-500">
-            Lore
-          </div>,
-          <div key="2" className="h-svh w-full bg-blue-500">
-            Lore
-          </div>,
-        ]}
-      />
-      <div className="h-[1/3vh] w-full bg-green-500"></div>
-      <div className="h-[1/3vh] w-full bg-yellow-500"></div>
+      boxy
     </div>
   );
 }
